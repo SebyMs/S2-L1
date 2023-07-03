39364cb9e361c1df3c5dfa558e8334c87ef43c80
SebyMs <sebastiano.monello02@gmail.com>
2023-07-03 17:21:37 +0200

    Datatype

--- D1.js
+++ D1.js
@@ -16,6 +16,8 @@ REGOLE
 //   3 datatype é boolean che comprende solo due valori vero o falso
 //   4 undefined é un datatype affibbiato ad una variabile senza valore
 //   5 null é un datatype affibbiato intenzionalmente ad una varibile per non darle un valore
+// 6 Bignlt serve per rappresentare numeri molto grandi che non posso essere rappresentanti in 64 bit
+// 7 datatype simbol é usato per rappresenatre simboli in input e output senza farli collidere con altro codice si puo definire con un incapsulazione
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.

8884f074394758106450349743927731a6084f9e
SebyMs <sebastiano.monello02@gmail.com>
2023-07-03 17:56:11 +0200

    Aggiunta testo a risultati variabili

--- D1.js
+++ D1.js
@@ -9,7 +9,7 @@ REGOLE
 
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
-  
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 //   1 datatype é string che é una semplice linea di testo composta da caratteri
 //   2 datatype é number che sono semplicimente numeri sia interi che con virgola
@@ -25,21 +25,21 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 let name = "Sebastiano Monello";
-console.log(name);
+console.log("Il mio nome é ", name);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-console.log(12 + 20);
+console.log("La somma di 12+20 é", 12 + 20);
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let numero = 12;
-console.log(numero);
+let x = 12;
+console.log("La variabile x é ", x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -48,17 +48,17 @@ console.log(numero);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 name = "Monello";
-console.log(name);
+console.log("Variabile name modificata é ", name);
 const nome = "Sebastiano Monello";
-console.log(nome);
+console.log("La varibile const non puó essere modificata");
 // nome='Monello'impossibile cambiare variabile all attributo const perché essendo un datatype constante generera errore
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let x = 12;
-console.log(4 - x);
+x = 12;
+console.log("4 - variabile x é ", 4 - x);
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
